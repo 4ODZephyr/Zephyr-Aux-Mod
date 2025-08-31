0e7ebba7ed90f50b77653c013b5a5e0b5c12842d
Lupus590 <9790321+Lupus590@users.noreply.github.com>
2025-08-31 14:17:30 +0100

    Scaffold for TCP AAX

---
+++ addons/aax/TCP/config.cpp
@@ -0,0 +1,16 @@
+class CfgPatches
+{
+	class 4ODZ_AAX_TCP
+	{
+		name = "AAX TCP";
+		author = "RoutOfWolves";
+		units[] = {};
+		weapons[] = {};
+		requiredAddons[] =
+		{
+			"aceax_gearinfo",
+			// TODO: what's the TCP addon name?
+		};
+		skipWhenMissingDependencies = 1;
+	};
+};
