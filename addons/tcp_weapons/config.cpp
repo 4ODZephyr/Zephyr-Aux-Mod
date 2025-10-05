be0fe4a86b69754cabd4a18c62a1fae3aaae284f
Lupus590 <9790321+Lupus590@users.noreply.github.com>
2025-10-05 13:48:02 +0100

    Improve dependencies in zephyr tcp weapons PBO (#20)

--- addons/tcp_weapons/config.cpp
+++ addons/tcp_weapons/config.cpp
@@ -3,7 +3,7 @@ class CfgPatches
 	class Zephyr_TCP_Weapons
 	{
 		addonRootClass = "Zephyr_Core";
-		name = "TCP Weapons";
+		name = "Zephyr - TCP Weapons";
 		author = "Lupus590";
 		units[] = {};
 		weapons[] = {};
@@ -11,13 +11,32 @@ class CfgPatches
 		{
 			"Zephyr_Core",
 			"TCP_Weapons_Ammo_40",
+			"OPTRE_Weapons_Ammo",
 		};
+		skipWhenMissingDependencies = 1;
 	};
 };
 class CfgMagazineWells
 {
 	class TCP_40_1
 	{
-		TCP_Weapons_Ammo_40_Smoke[] += {"M319_Smoke","M319_Smoke_Red","M319_Smoke_Green","M319_Smoke_Orange"};
+		TCP_Weapons_Ammo_40_Smoke[] += {
+			// normal smokes
+			"1Rnd_Smoke_Grenade_shell",
+			"1Rnd_SmokeRed_Grenade_shell",
+			"1Rnd_SmokeGreen_Grenade_shell",
+			"1Rnd_SmokeYellow_Grenade_shell",
+			"1Rnd_SmokePurple_Grenade_shell",
+			"1Rnd_SmokeBlue_Grenade_shell",
+			"1Rnd_SmokeOrange_Grenade_shell",
+			// Impact smokes
+			"OPTRE_1Rnd_Smoke_Grenade_shell",
+			"OPTRE_1Rnd_SmokeRed_Grenade_shell",
+			"OPTRE_1Rnd_SmokeGreen_Grenade_shell",
+			"OPTRE_1Rnd_SmokeYellow_Grenade_shell",
+			"OPTRE_1Rnd_SmokePurple_Grenade_shell",
+			"OPTRE_1Rnd_SmokeBlue_Grenade_shell",
+			"OPTRE_1Rnd_SmokeOrange_Grenade_shell",
+		};
 	};
 };
