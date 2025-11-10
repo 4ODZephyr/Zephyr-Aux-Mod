87044a9087e9f2f80aa58d9ffdda5befcae7506c
Lupus590 <9790321+Lupus590@users.noreply.github.com>
2025-11-10 14:52:02 +0000

    more boilerplate

---
+++ addons/aax/patches/tcp/README.md
@@ -0,0 +1,13 @@
+# AAX TCP Addon
+
+Adds new features or content as part of the Zephyr Aux Mod.
+
+## Usage
+
+Describe the purpose and usage of this addon here.
+
+## Requirements
+
+- Zephyr Core addon
+- Ace Arsenal Extended
+- The Cole Protocol
---
+++ addons/aax/patches/tcp/config.cpp
@@ -0,0 +1,15 @@
+class CfgPatches
+{
+	class Zephyr_AAX_TCP
+	{
+		addonRootClass = "Zephyr_AAX";
+		name = "Zephyr - AAX - TCP";
+		author = "Lupus590";
+		units[] = {};
+		weapons[] = {};
+		requiredAddons[] =
+		{
+			// TODO: need to find from TCP
+		};
+	};
+};
