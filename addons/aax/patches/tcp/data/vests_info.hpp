6e50f33e0d88c26ac3a27bc108b9ac5224719cd5
Lupus590 <9790321+Lupus590@users.noreply.github.com>
2025-11-13 15:30:04 +0000

    part works part doesn't
    committing just the part that works

--- addons/aax/patches/tcp/data/vests_info.hpp
+++ addons/aax/patches/tcp/data/vests_info.hpp
@@ -1,65 +1,52 @@
-#define Macro_Zephyr_AAX_TCP_CBBAS_M43A(ArgCamo) \
-	class TCP_V_M43A_Light_##ArgCamo## { \
-		model = "Zephyr_AAX_TCP_CBBAS_M43A"; \
-		Camo = #ArgCamo; \
-		ShoulderArmor = "ShoulderNone"; \
-		ExtraArmor = "ExtraNone"; \
+#define Macro_Zephyr_AAX_TCP_CBBAS_M43A_Body(ArgCamo, ArgShoulderArmor, ArgExtraArmor) \
+	model = "Zephyr_AAX_TCP_CBBAS_M43A"; \
+	Camo = #ArgCamo; \
+	ShoulderArmor = #ArgShoulderArmor; \
+	ExtraArmor = #ArgExtraArmor;
+// end Macro_Zephyr_AAX_TCP_CBBAS_M43A_Body
+
+#define Macro_Zephyr_AAX_TCP_CBBAS_M43A_AllForCamo(ArgCamo) \
+	class TCP_V_M43A_Light_##ArgCamo## \
+	{ \
+		Macro_Zephyr_AAX_TCP_CBBAS_M43A_Body(ArgCamo,ShoulderNone,ExtraNone) \
 	}; \
-	class TCP_V_M43A_BaseSec_1_##ArgCamo## { \
-		model = "Zephyr_AAX_TCP_CBBAS_M43A"; \
-		Camo = #ArgCamo; \
-		ShoulderArmor = "ShoulderBaseSecurity"; \
-		ExtraArmor = "ExtraNone"; \
+	class TCP_V_M43A_BaseSec_1_##ArgCamo## \
+	{ \
+		Macro_Zephyr_AAX_TCP_CBBAS_M43A_Body(ArgCamo,ShoulderBaseSecurity,ExtraNone) \
 	}; \
-	class TCP_V_M43A_BaseSec_2_##ArgCamo## { \
-		model = "Zephyr_AAX_TCP_CBBAS_M43A"; \
-		Camo = #ArgCamo; \
-		ShoulderArmor = "ShoulderBaseSecurity"; \
-		ExtraArmor = "ExtraThighs"; \
+	class TCP_V_M43A_BaseSec_2_##ArgCamo## \
+	{ \
+		Macro_Zephyr_AAX_TCP_CBBAS_M43A_Body(ArgCamo,ShoulderBaseSecurity,ExtraThighs) \
 	}; \
-	class TCP_V_M43A_BaseSec_3_##ArgCamo## { \
-		model = "Zephyr_AAX_TCP_CBBAS_M43A"; \
-		Camo = #ArgCamo; \
-		ShoulderArmor = "ShoulderBaseSecurity"; \
-		ExtraArmor = "ExtraThighsShins"; \
+	class TCP_V_M43A_BaseSec_3_##ArgCamo## \
+	{ \
+		Macro_Zephyr_AAX_TCP_CBBAS_M43A_Body(ArgCamo,ShoulderBaseSecurity,ExtraThighsShins) \
 	}; \
-	class TCP_V_M43A_GungnirS_1_##ArgCamo## { \
-		model = "Zephyr_AAX_TCP_CBBAS_M43A"; \
-		Camo = #ArgCamo; \
-		ShoulderArmor = "ShoulderGungnirSmall"; \
-		ExtraArmor = "ExtraNone"; \
+	class TCP_V_M43A_GungnirS_1_##ArgCamo## \
+	{ \
+		Macro_Zephyr_AAX_TCP_CBBAS_M43A_Body(ArgCamo,ShoulderGungnirSmall,ExtraNone) \
 	}; \
-	class TCP_V_M43A_GungnirS_2_##ArgCamo## { \
-		model = "Zephyr_AAX_TCP_CBBAS_M43A"; \
-		Camo = #ArgCamo; \
-		ShoulderArmor = "ShoulderGungnirSmall"; \
-		ExtraArmor = "ExtraThighs"; \
+	class TCP_V_M43A_GungnirS_2_##ArgCamo## \
+	{ \
+		Macro_Zephyr_AAX_TCP_CBBAS_M43A_Body(ArgCamo,ShoulderGungnirSmall,ExtraThighs) \
 	}; \
-	class TCP_V_M43A_GungnirS_3_##ArgCamo## { \
-		model = "Zephyr_AAX_TCP_CBBAS_M43A"; \
-		Camo = #ArgCamo; \
-		ShoulderArmor = "ShoulderGungnirSmall"; \
-		ExtraArmor = "ExtraThighsShins"; \
+	class TCP_V_M43A_GungnirS_3_##ArgCamo## \
+	{ \
+		Macro_Zephyr_AAX_TCP_CBBAS_M43A_Body(ArgCamo,ShoulderGungnirSmall,ExtraThighsShins) \
 	}; \
-	class TCP_V_M43A_GungnirL_1_##ArgCamo## { \
-		model = "Zephyr_AAX_TCP_CBBAS_M43A"; \
-		Camo = #ArgCamo; \
-		ShoulderArmor = "ShoulderGungnirLarge"; \
-		ExtraArmor = "ExtraNone"; \
+	class TCP_V_M43A_GungnirL_1_##ArgCamo## \
+	{ \
+		Macro_Zephyr_AAX_TCP_CBBAS_M43A_Body(ArgCamo,ShoulderGungnirLarge,ExtraNone) \
 	}; \
-	class TCP_V_M43A_GungnirL_2_##ArgCamo## { \
-		model = "Zephyr_AAX_TCP_CBBAS_M43A"; \
-		Camo = #ArgCamo; \
-		ShoulderArmor = "ShoulderGungnirLarge"; \
-		ExtraArmor = "ExtraThighs"; \
+	class TCP_V_M43A_GungnirL_2_##ArgCamo## \
+	{ \
+		Macro_Zephyr_AAX_TCP_CBBAS_M43A_Body(ArgCamo,ShoulderGungnirLarge,ExtraThighs) \
 	}; \
-	class TCP_V_M43A_GungnirL_3_##ArgCamo## { \
-		model = "Zephyr_AAX_TCP_CBBAS_M43A"; \
-		Camo = #ArgCamo; \
-		ShoulderArmor = "ShoulderGungnirLarge"; \
-		ExtraArmor = "ExtraThighsShins"; \
+	class TCP_V_M43A_GungnirL_3_##ArgCamo## \
+	{ \
+		Macro_Zephyr_AAX_TCP_CBBAS_M43A_Body(ArgCamo,ShoulderGungnirLarge,ExtraThighsShins) \
 	};
-// End Macro_Zephyr_AAX_TCP_CBBAS_M43A
+// end Macro_Zephyr_AAX_TCP_CBBAS_M43A
 
 #define Macro_Zephyr_AAX_TCP_CBBAS_M43D(ArgCamo) \
 	class TCP_V_M43D_Light_##ArgCamo## { \
