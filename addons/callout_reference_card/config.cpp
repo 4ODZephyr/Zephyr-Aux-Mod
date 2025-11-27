73666a91d01f0c9bb9504d6e838d483fcf0108ee
Lupus590 <9790321+Lupus590@users.noreply.github.com>
2025-11-27 21:36:29 +0000

    boilerplate

---
+++ addons/callout_reference_card/config.cpp
@@ -0,0 +1,17 @@
+class CfgPatches
+{
+	class Zephyr_Callout_Reference_Card
+	{
+		addonRootClass = "Zephyr_Core";
+		name = "Zephyr - Callout Reference Card";
+		author = "Lupus590";
+		units[] = {};
+		weapons[] = {};
+		requiredAddons[] =
+		{
+			"Zephyr_Core",
+			"Zephyr_Unit_Patch",
+		};
+	};
+};
+
