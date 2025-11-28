068d70b03bb24f464fe9bc57bb51e367498e338c
Lupus590 <9790321+Lupus590@users.noreply.github.com>
2025-11-28 21:36:21 +0000

    wip

--- addons/callout_reference_card/config.cpp
+++ addons/callout_reference_card/config.cpp
@@ -18,95 +18,94 @@ class CfgPatches
 };
 
 class CfgWeapons {
-    class ACE_ItemCore;
-    class CBA_MiscItem_ItemInfo;
+	class ACE_ItemCore;
+	class CBA_MiscItem_ItemInfo;
 
-    class Zephyr_CalloutReferenceCard: ACE_ItemCore {
-        author = "Lupus590";
-        scope = 2;
-        displayName = "Callout Reference Card";
-        descriptionShort = "A handy reference for Zephyr specific tactical callouts.";
-        picture = "\z\zephyr\addons\unit_patch\zephyr_logo.paa";
-        icon = "iconObject_circle"; // "\z\zephyr\addons\unit_patch\zephyr_logo.paa";
-        mapSize = 0.034;
-        ACE_isTool = 1;
+	class Zephyr_CalloutReferenceCard: ACE_ItemCore {
+		author = "Lupus590";
+		scope = 2;
+		displayName = "Callout Reference Card";
+		descriptionShort = "A handy reference for Zephyr specific tactical callouts.";
+		picture = "\z\zephyr\addons\unit_patch\zephyr_logo.paa";
+		icon = "iconObject_circle"; // "\z\zephyr\addons\unit_patch\zephyr_logo.paa";
+		mapSize = 0.034;
+		ACE_isTool = 1;
 
-        class ItemInfo: CBA_MiscItem_ItemInfo {
-            mass = 0.1;
-        };
-    };
+		class ItemInfo: CBA_MiscItem_ItemInfo {
+			mass = 0.1;
+		};
+	};
 };
 
 
 class CfgVehicles {
-	/*
-    class Man;
-    class CAManBase: Man {
-        class ACE_Actions {
-            class ACE_Weapon {
-                class GVAR(copyRangeCard) {
-                    displayName = CSTRING(CopyRangeCard);
-                    distance = 2.0;
-                    condition = QUOTE(_target call FUNC(canCopy));
-                    statement = QUOTE(_target call FUNC(updateClassNames));
-                    icon = QPATHTOF(UI\RangeCard_Icon.paa);
-                };
-            };
-        };
-        class ACE_SelfActions {
-            class ACE_Equipment {
-                class GVAR(open) {
-                    displayName = CSTRING(OpenRangeCard);
-                    condition = QUOTE(call FUNC(canShow) && !GVAR(RangeCardOpened));
-                    statement = QUOTE(false call FUNC(openRangeCard));
-                    showDisabled = 0;
-                    icon = QPATHTOF(UI\RangeCard_Icon.paa);
-                    exceptions[] = {"notOnMap", "isNotInside"};
-                    class GVAR(openCopy) {
-                        displayName = CSTRING(OpenRangeCardCopy);
-                        condition = QUOTE(call FUNC(canShowCopy) && !GVAR(RangeCardOpened));
-                        statement = QUOTE(true call FUNC(openRangeCard));
-                        showDisabled = 0;
-                        icon = QPATHTOF(UI\RangeCard_Icon.paa);
-                        exceptions[] = {"notOnMap", "isNotInside"};
-                    };
-                    class GVAR(makeCopy) {
-                        displayName = CSTRING(CopyRangeCard);
-                        condition = QUOTE(call FUNC(canShow) && !GVAR(RangeCardOpened));
-                        statement = QUOTE(GVAR(zeroRangeCopy)=GVAR(zeroRange); GVAR(boreHeightCopy)=GVAR(boreHeight); GVAR(ammoClassCopy)=GVAR(ammoClass); GVAR(magazineClassCopy)=GVAR(magazineClass); GVAR(weaponClassCopy)=GVAR(weaponClass););
-                        showDisabled = 0;
-                        icon = QPATHTOF(UI\RangeCard_Icon.paa);
-                        exceptions[] = {"notOnMap", "isNotInside"};
-                    };
-                };
-            };
-        };
-    };
-	*/
+
+	class Man;
+	class CAManBase: Man {
+		class ACE_Actions {
+			class ACE_Weapon {
+				class Zephyr_CopyCalloutReferenceCard {
+					displayName = "Copy Callout Reference Card";
+					distance = 2.0;
+					//condition = QUOTE(_target call FUNC(canCopy));
+					//statement = QUOTE(_target call FUNC(updateClassNames));
+					icon = "\z\zephyr\addons\unit_patch\zephyr_logo.paa";
+				};
+			};
+		};
+		class ACE_SelfActions {
+			class ACE_Equipment {
+				class Zephyr_OpenCalloutReferenceCard {
+					displayName = "Open Callout Reference Card";
+					//condition = QUOTE(call FUNC(canShow) && !GVAR(RangeCardOpened));
+					//statement = QUOTE(false call FUNC(openRangeCard));
+					showDisabled = 0;
+					icon = "\z\zephyr\addons\unit_patch\zephyr_logo.paa";
+					exceptions[] = {"notOnMap", "isNotInside"};
+					/*class GVAR(openCopy) {
+						displayName = CSTRING(OpenRangeCardCopy);
+						condition = QUOTE(call FUNC(canShowCopy) && !GVAR(RangeCardOpened));
+						statement = QUOTE(true call FUNC(openRangeCard));
+						showDisabled = 0;
+						icon = "\z\zephyr\addons\unit_patch\zephyr_logo.paa";
+						exceptions[] = {"notOnMap", "isNotInside"};
+					};*/
+					class Zephyr_MakeCopyCalloutReferenceCard {
+						displayName = "Copy Callout Reference Card";
+						//condition = QUOTE(call FUNC(canShow) && !GVAR(RangeCardOpened));
+						//statement = QUOTE(GVAR(zeroRangeCopy)=GVAR(zeroRange); GVAR(boreHeightCopy)=GVAR(boreHeight); GVAR(ammoClassCopy)=GVAR(ammoClass); GVAR(magazineClassCopy)=GVAR(magazineClass); GVAR(weaponClassCopy)=GVAR(weaponClass););
+						showDisabled = 0;
+						icon = "\z\zephyr\addons\unit_patch\zephyr_logo.paa";
+						exceptions[] = {"notOnMap", "isNotInside"};
+					};
+				};
+			};
+		};
+	};
 
 	#define MACRO_ADDITEM(ITEM,COUNT) class _xx_##ITEM { \
-    name = #ITEM; \
-    count = COUNT; \
+	name = #ITEM; \
+	count = COUNT; \
 }
 
-    class Item_Base_F;
-    class Zephyr_Item_CalloutReferenceCard: Item_Base_F {
-        displayName = "Callout Reference Card";
-        author = "Lupus590";
-        scope = 2;
-        scopeCurator = 2;
-        vehicleClass = "Items";
-        class TransportItems {
-            MACRO_ADDITEM(Zephyr_CalloutReferenceCard,1);
-        };
-    };
+	class Item_Base_F;
+	class Zephyr_Item_CalloutReferenceCard: Item_Base_F {
+		displayName = "Callout Reference Card";
+		author = "Lupus590";
+		scope = 2;
+		scopeCurator = 2;
+		vehicleClass = "Items";
+		class TransportItems {
+			MACRO_ADDITEM(Zephyr_CalloutReferenceCard,1);
+		};
+	};
 
-    class Box_NATO_Support_F;
-    class ACE_Box_Misc: Box_NATO_Support_F {
-        class TransportItems {
-            MACRO_ADDITEM(Zephyr_CalloutReferenceCard,6);
-        };
-    };
+	class Box_NATO_Support_F;
+	class ACE_Box_Misc: Box_NATO_Support_F {
+		class TransportItems {
+			MACRO_ADDITEM(Zephyr_CalloutReferenceCard,6);
+		};
+	};
 };
 
 
