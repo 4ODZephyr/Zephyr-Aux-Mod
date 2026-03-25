cebadbbe2ab8707f5598e6c76f1f7ddcfccc372c
Lupus590 <9790321+Lupus590@users.noreply.github.com>
2026-03-25 23:56:02 +0000

    wip

--- addons/tcp_ranks/config.cpp
+++ addons/tcp_ranks/config.cpp
@@ -24,114 +24,130 @@ class CfgTCPRanks
 		{
 			class R01
 			{
-				displayName = "R1 (Cadet)";
-				black = "\cp_ranks\data\decals\ranks\R01_CDT_CA.paa";
-				white = "\cp_ranks\data\decals\ranks\R01_CDT_CA.paa";
+				displayName = "Cadet";
+				displayNameShort = "CDT";
+				black = "\z\zephyr\addons\tcp_ranks\data\decals\ranks\R01_CDT_CA.paa";
+				white = "\z\zephyr\addons\tcp_ranks\data\decals\ranks\R01_CDT_CA.paa";
 				//material = "\TCP\Data\Decals\Ranks\UNICOM\rank_E1.rvmat";
 			};
 			class R02
 			{
-				displayName = "R2 (Private)";
-				black = "\cp_ranks\data\decals\ranks\R02_PVT_CA.paa";
-				white = "\cp_ranks\data\decals\ranks\R02_PVT_CA.paa";
+				displayName = "Private";
+				displayNameShort = "PVT";
+				black = "\z\zephyr\addons\tcp_ranks\data\decals\ranks\R02_PVT_CA.paa";
+				white = "\z\zephyr\addons\tcp_ranks\data\decals\ranks\R02_PVT_CA.paa";
 				//material = "\TCP\Data\Decals\Ranks\UNICOM\rank_E1.rvmat";
 			};
 			class R03
 			{
-				displayName = "R3 (Private First Class)";
-				black = "\cp_ranks\data\decals\ranks\R03_PFC_CA.paa";
-				white = "\cp_ranks\data\decals\ranks\R03_PFC_CA.paa";
+				displayName = "Private First Class";
+				displayNameShort = "PFC";
+				black = "\z\zephyr\addons\tcp_ranks\data\decals\ranks\R03_PFC_CA.paa";
+				white = "\z\zephyr\addons\tcp_ranks\data\decals\ranks\R03_PFC_CA.paa";
 				//material = "\TCP\Data\Decals\Ranks\UNICOM\rank_E1.rvmat";
 			};
 			class R04
 			{
-				displayName = "R4 (Lance Corporal)";
-				black = "\cp_ranks\data\decals\ranks\R04_LCPL_CA.paa";
-				white = "\cp_ranks\data\decals\ranks\R04_LCPL_CA.paa";
+				displayName = "Lance Corporal";
+				displayNameShort = "LCPL";
+				black = "\z\zephyr\addons\tcp_ranks\data\decals\ranks\R04_LCPL_CA.paa";
+				white = "\z\zephyr\addons\tcp_ranks\data\decals\ranks\R04_LCPL_CA.paa";
 				//material = "\TCP\Data\Decals\Ranks\UNICOM\rank_E1.rvmat";
 			};
 			class R05
 			{
-				displayName = "R5 (Specialist)";
-				black = "\cp_ranks\data\decals\ranks\R05_SPC_CA.paa";
-				white = "\cp_ranks\data\decals\ranks\R05_SPC_CA.paa";
+				displayName = "Specialist";
+				displayNameShort = "SPC";
+				black = "\z\zephyr\addons\tcp_ranks\data\decals\ranks\R05_SPC_CA.paa";
+				white = "\z\zephyr\addons\tcp_ranks\data\decals\ranks\R05_SPC_CA.paa";
 				//material = "\TCP\Data\Decals\Ranks\UNICOM\rank_E1.rvmat";
 			};
 			class R06
 			{
-				displayName = "R6 (Warrent Officer)";
-				black = "\cp_ranks\data\decals\ranks\R06_WO_CA.paa";
-				white = "\cp_ranks\data\decals\ranks\R06_WO_CA.paa";
+				displayName = "Warrent Officer";
+				displayNameShort = "WO";
+				black = "\z\zephyr\addons\tcp_ranks\data\decals\ranks\R06_WO_CA.paa";
+				white = "\z\zephyr\addons\tcp_ranks\data\decals\ranks\R06_WO_CA.paa";
 				//material = "\TCP\Data\Decals\Ranks\UNICOM\rank_E1.rvmat";
 			};
 			class R07
 			{
-				displayName = "R7 (Chief Warrent Officer)";
-				black = "\cp_ranks\data\decals\ranks\R07_CWO_CA.paa";
-				white = "\cp_ranks\data\decals\ranks\R07_CWO_CA.paa";
+				displayName = "Chief Warrent Officer";
+				displayNameShort = "CWO";
+				black = "\z\zephyr\addons\tcp_ranks\data\decals\ranks\R07_CWO_CA.paa";
+				white = "\z\zephyr\addons\tcp_ranks\data\decals\ranks\R07_CWO_CA.paa";
 				//material = "\TCP\Data\Decals\Ranks\UNICOM\rank_E1.rvmat";
 			};
 			class R08
 			{
-				displayName = "R8 (Petty Officer)";
-				black = "\cp_ranks\data\decals\ranks\R08_PO_CA.paa";
-				white = "\cp_ranks\data\decals\ranks\R08_PO_CA.paa";
+				displayName = "Petty Officer";
+				displayNameShort = "PO";
+				black = "\z\zephyr\addons\tcp_ranks\data\decals\ranks\R08_PO_CA.paa";
+				white = "\z\zephyr\addons\tcp_ranks\data\decals\ranks\R08_PO_CA.paa";
 				//material = "\TCP\Data\Decals\Ranks\UNICOM\rank_E1.rvmat";
 			};
 			class R09
 			{
-				displayName = "R9 (Chief Petty Officer)";
-				black = "\cp_ranks\data\decals\ranks\R09_CPO_CA.paa";
-				white = "\cp_ranks\data\decals\ranks\R09_CPO_CA.paa";
+				displayName = "Chief Petty Officer";
+				displayNameShort = "CPO";
+				black = "\z\zephyr\addons\tcp_ranks\data\decals\ranks\R09_CPO_CA.paa";
+				white = "\z\zephyr\addons\tcp_ranks\data\decals\ranks\R09_CPO_CA.paa";
 				//material = "\TCP\Data\Decals\Ranks\UNICOM\rank_E1.rvmat";
 			};
 			class R10
 			{
-				displayName = "R10 (Senior Chief Petty Officer)";
-				black = "\cp_ranks\data\decals\ranks\R10_SCPO_CA.paa";
-				white = "\cp_ranks\data\decals\ranks\R10_SCPO_CA.paa";
+				displayName = "Senior Chief Petty Officer";
+				displayNameShort = "SCPO";
+				black = "\z\zephyr\addons\tcp_ranks\data\decals\ranks\R10_SCPO_CA.paa";
+				white = "\z\zephyr\addons\tcp_ranks\data\decals\ranks\R10_SCPO_CA.paa";
 				//material = "\TCP\Data\Decals\Ranks\UNICOM\rank_E1.rvmat";
 			};
 			class L05
 			{
-				displayName = "L5 (Corporal)";
-				black = "\cp_ranks\data\decals\ranks\L05_CPL_CA.paa";
-				white = "\cp_ranks\data\decals\ranks\L05_CPL_CA.paa";
+				displayName = "Corporal";
+				displayNameShort = "CPL";
+				black = "\z\zephyr\addons\tcp_ranks\data\decals\ranks\L05_CPL_CA.paa";
+				white = "\z\zephyr\addons\tcp_ranks\data\decals\ranks\L05_CPL_CA.paa";
 				//material = "\TCP\Data\Decals\Ranks\UNICOM\rank_E1.rvmat";
 			};
 			class L06
 			{
-				displayName = "L6 (Sergeant)";
-				black = "\cp_ranks\data\decals\ranks\L06_SGT_CA.paa";
-				white = "\cp_ranks\data\decals\ranks\L06_SGT_CA.paa";
+				displayName = "Sergeant";
+				displayNameShort = "SGT";
+				black = "\z\zephyr\addons\tcp_ranks\data\decals\ranks\L06_SGT_CA.paa";
+				white = "\z\zephyr\addons\tcp_ranks\data\decals\ranks\L06_SGT_CA.paa";
 				//material = "\TCP\Data\Decals\Ranks\UNICOM\rank_E1.rvmat";
 			};
 			class L07
 			{
-				displayName = "L7 (Staff Sergeant)";
-				black = "\cp_ranks\data\decals\ranks\L07_SSG_CA.paa";
-				white = "\cp_ranks\data\decals\ranks\L07_SSG_CA.paa";
+				displayName = "Staff Sergeant";
+				displayNameShort = "SSGT";
+				black = "\z\zephyr\addons\tcp_ranks\data\decals\ranks\L07_SSGT_CA.paa";
+				white = "\z\zephyr\addons\tcp_ranks\data\decals\ranks\L07_SSGT_CA.paa";
 				//material = "\TCP\Data\Decals\Ranks\UNICOM\rank_E1.rvmat";
 			};
 			class L08
 			{
-				displayName = "L8 (2nd Lieutenant)";
-				black = "\cp_ranks\data\decals\ranks\L08_2LT_CA.paa";
-				white = "\cp_ranks\data\decals\ranks\L08_2LT_CA.paa";
+				displayName = "2nd Lieutenant";
+				displayNameShort = "2LT";
+				black = "\z\zephyr\addons\tcp_ranks\data\decals\ranks\L08_2LT_CA.paa";
+				white = "\z\zephyr\addons\tcp_ranks\data\decals\ranks\L08_2LT_CA.paa";
 				//material = "\TCP\Data\Decals\Ranks\UNICOM\rank_E1.rvmat";
 			};
 			class L09
 			{
-				displayName = "L9 (1st Lieutenant)";
-				black = "\cp_ranks\data\decals\ranks\L09_1LT_CA.paa";
-				white = "\cp_ranks\data\decals\ranks\L09_1LT_CA.paa";
+				displayName = "1st Lieutenant";
+				displayNameShort = "1LT";
+				black = "\z\zephyr\addons\tcp_ranks\data\decals\ranks\L09_1LT_CA.paa";
+				white = "\z\zephyr\addons\tcp_ranks\data\decals\ranks\L09_1LT_CA.paa";
 				//material = "\TCP\Data\Decals\Ranks\UNICOM\rank_E1.rvmat";
 			};
 			class L10
 			{
-				displayName = "L10 (Captain)";
-				black = "\cp_ranks\data\decals\ranks\L10_CPT_CA.paa";
-				white = "\cp_ranks\data\decals\ranks\L10_CPT_CA.paa";
+				displayName = "Captain";
+				displayNameShort = "CPT";
+				black = "\z\zephyr\addons\tcp_ranks\data\decals\ranks\L10_CPT_CA.paa";
+				white = "\z\zephyr\addons\tcp_ranks\data\decals\ranks\L10_CPT_CA.paa";
 				//material = "\TCP\Data\Decals\Ranks\UNICOM\rank_E1.rvmat";
 			};
 		};
