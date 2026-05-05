66fdccb3f4b830b4ea1528c5f4378570a865ff45
Lupus590 <9790321+Lupus590@users.noreply.github.com>
2026-05-06 00:57:31 +0100

    do the other optics

--- addons/tweaks/tcp_optics_nvg/config.cpp
+++ addons/tweaks/tcp_optics_nvg/config.cpp
@@ -11,6 +11,9 @@ class CfgPatches
 		{
 			"Zephyr_Core",
 			"TCP_Weapons_Acc_Optic_KFA_M6G2",
+			"TCP_Weapons_Acc_Optic_EVOSD",
+			"TCP_Weapons_Acc_Optic_KFA_M6G",
+			"TCP_Weapons_ins_Acc_Optic_M43RCO",
 		};
 		skipWhenMissingDependencies = 1;
 	};
@@ -29,6 +32,54 @@ class CfgWeapons
 			class OpticsModes
 			{
 				class 2D
+				{
+					visionMode[] = {};
+				};
+			};
+		};
+	};
+	class ItemCore;
+	class InventoryOpticsItem_Base_F;
+	class TCP_optic_EVOSD: ItemCore
+	{
+		class ItemInfo: InventoryOpticsItem_Base_F
+		{
+			class OpticsModes
+			{
+				class EVOSD
+				{
+					visionMode[] = {};
+				};
+			};
+		};
+	};
+
+	class ItemCore;
+	class InventoryOpticsItem_Base_F;
+	class TCP_OpticsMode_Base_3D;
+	class TCP_optic_KFA_M6G: ItemCore
+	{
+		class ItemInfo: InventoryOpticsItem_Base_F
+		{
+			class OpticsModes
+			{
+				class 2D
+				{
+					visionMode[] = {};
+				};
+			};
+		};
+	};
+	class TCP_OpticsMode_Base_Irons;
+	class ItemCore;
+	class InventoryOpticsItem_Base_F;
+	class TCP_optic_M43RCO: ItemCore
+	{
+		class ItemInfo: InventoryOpticsItem_Base_F
+		{
+			class OpticsModes
+			{
+				class EVOSD
 				{
 					visionMode[] = {};
 				};
