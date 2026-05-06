d72959268117d1ce4c2c7898ac58dc49e1aed825
Lupus590 <9790321+Lupus590@users.noreply.github.com>
2026-05-06 01:22:24 +0100

    make nvgs work in tcp optics (#93)

---
+++ addons/tweaks/tcp_optics_nvg/README.md
@@ -0,0 +1,3 @@
+# TCP Optics NVG
+
+Adds user NVG support to optics that don't have night vision built into them. Bring your own vision enhancements.
---
+++ addons/tweaks/tcp_optics_nvg/config.cpp
@@ -0,0 +1,122 @@
+class CfgPatches
+{
+	class Zephyr_Tweaks_TCP_Optics_NVG
+	{
+		addonRootClass = "Zephyr_Core";
+		name = "Zephyr - Tweaks - TCP Optics NVG";
+		author = "Lupus590";
+		units[] = {};
+		weapons[] = {};
+		requiredAddons[] =
+		{
+			"Zephyr_Core",
+			"TCP_Weapons_Acc_Optic_KFA_M6G2",
+			"TCP_Weapons_Acc_Optic_EVOSD",
+			"TCP_Weapons_Acc_Optic_KFA_M6G",
+			"TCP_Weapons_ins_Acc_Optic_M43RCO",
+			"TCP_Weapons_ins_Acc_Optic_M27RCO",
+			"TCP_Weapons_Ins_Acc_Optic_PRO",
+		};
+		skipWhenMissingDependencies = 1;
+	};
+};
+
+
+class CfgWeapons
+{
+	class ItemCore;
+	class InventoryOpticsItem_Base_F;
+	class TCP_OpticsMode_Base_3D;
+	class TCP_optic_KFA_M6G2: ItemCore
+	{
+		class ItemInfo: InventoryOpticsItem_Base_F
+		{
+			class OpticsModes
+			{
+				class 2D
+				{
+					visionMode[] = {};
+				};
+			};
+		};
+	};
+	//class ItemCore;
+	//class InventoryOpticsItem_Base_F;
+	class TCP_optic_EVOSD: ItemCore
+	{
+		class ItemInfo: InventoryOpticsItem_Base_F
+		{
+			class OpticsModes
+			{
+				class EVOSD
+				{
+					visionMode[] = {};
+				};
+			};
+		};
+	};
+
+	//class ItemCore;
+	//class InventoryOpticsItem_Base_F;
+	//class TCP_OpticsMode_Base_3D;
+	class TCP_optic_KFA_M6G: ItemCore
+	{
+		class ItemInfo: InventoryOpticsItem_Base_F
+		{
+			class OpticsModes
+			{
+				class 2D
+				{
+					visionMode[] = {};
+				};
+			};
+		};
+	};
+	//class TCP_OpticsMode_Base_Irons;
+	//class ItemCore;
+	//class InventoryOpticsItem_Base_F;
+	class TCP_optic_M43RCO: ItemCore
+	{
+		class ItemInfo: InventoryOpticsItem_Base_F
+		{
+			class OpticsModes
+			{
+				class EVOSD
+				{
+					visionMode[] = {};
+				};
+			};
+		};
+	};
+	//class TCP_OpticsMode_Base_Irons;
+	//class ItemCore;
+	//class InventoryOpticsItem_Base_F;
+	class TCP_optic_M27RCO: ItemCore
+	{
+		class ItemInfo: InventoryOpticsItem_Base_F
+		{
+			class OpticsModes
+			{
+				class EVOSD
+				{
+					visionMode[] = {};
+				};
+			};
+		};
+	};
+	//class ItemCore;
+	//class InventoryOpticsItem_Base_F;
+	class TCP_optic_PRO: ItemCore
+	{
+		class ItemInfo: InventoryOpticsItem_Base_F
+		{
+			class OpticsModes
+			{
+				class PRO_2D
+				{
+					visionMode[] = {};
+				};
+			};
+		};
+	};
+};
